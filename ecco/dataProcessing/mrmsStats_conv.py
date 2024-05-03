b07a600355ea92f13091da5aa2de92bf4cc8e8a1
dixon <dixon@ucar.edu>
2024-05-03 15:22:26 -0600

    Statistics for convectivity

---
+++ ecco/dataProcessing/mrmsStats_conv.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python3
+
+#===========================================================================
+#
+# Process MRMS ECCO output
+#
+#===========================================================================
+
+import sys
+from optparse import OptionParser
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pl
+import netCDF4 as nc
+from matplotlib import dates
+import datetime
+import pathlib
+from matplotlib.dates import DateFormatter
+import pandas as pd
+import pickle
+import pyUtils.createFileList
+
+def main():
+
+    # globals
+
+    global options
+    global debug
+    global startTime
+    global endTime
+    global timeLimitsSet
+    timeLimitsSet = False
+    global figNum
+    figNum = 0
+    
+    # parse the command line
+
+    usage = "usage: %prog [options]"
+    parser = OptionParser(usage)
+    parser.add_option('--start',
+                      dest='startTime',
+                      default='2022 05 01 06 00 00',
+                      help='Start time for processing')
+    parser.add_option('--end',
+                      dest='endTime',
+                      default='2022 05 01 12 00 00',
+                      help='End time for processing')
+    parser.add_option('--inDir',
+                      dest='inDir',
+                      default='/scr/cirrus2/rsfdata/projects/nexrad-mrms/ecco_conus_terrain/',
+                      help='Directory for output data')
+    parser.add_option('--outDir',
+                      dest='outDir',
+                      default='/scr/cirrus2/rsfdata/projects/nexrad-mrms/statMats/',
+                      help='Directory for output data')
+        
+    
+    (options, args) = parser.parse_args()
+    
+    # time limits
+    year, month, day, hour, minute, sec = options.startTime.split()
+    startTime = datetime.datetime(int(year), int(month), int(day),
+                                  int(hour), int(minute), int(sec))
+
+    year, month, day, hour, minute, sec = options.endTime.split()
+    endTime = datetime.datetime(int(year), int(month), int(day),
+                                int(hour), int(minute), int(sec))
+
+    # Create file list
+    fileList=pyUtils.createFileList.createFileList(options.inDir, startTime, endTime, '%Y%m%d_%H%M%S.mdv.cf.nc', 1)
+    
+    # Read data
+    
+    timeBase=datetime.datetime(1970,1,1)
+    
+    readThis=nc.Dataset(fileList[0],'r')
+    lon=readThis.variables['x0'][:]
+    lat=readThis.variables['y0'][:]
+    
+    #initArray=np.zeros((24,len(lat),len(lon)))
+    StratLow=np.zeros((24,len(lat),len(lon)))
+    StratMid=np.zeros((24,len(lat),len(lon)))
+    StratHigh=np.zeros((24,len(lat),len(lon)))
+    Mixed=np.zeros((24,len(lat),len(lon)))
+    ConvElev=np.zeros((24,len(lat),len(lon)))
+    ConvShallow=np.zeros((24,len(lat),len(lon)))
+    ConvMid=np.zeros((24,len(lat),len(lon)))
+    ConvDeep=np.zeros((24,len(lat),len(lon)))
+    
+    CountAllHours=np.zeros((24,len(lat),len(lon)))
+    CountAllEcho=np.zeros((24,len(lat),len(lon)))
+            
+    for readFile in fileList:
+        print(readFile)
+        readThis=nc.Dataset(readFile,'r')
+        
+        readThis=nc.Dataset(readFile,'r')
+        timeIn=readThis.variables['time'][:]
+        time=timeBase+datetime.timedelta(seconds=timeIn[0])
+        
+        # Mean solar time
+        mst=np.empty(len(lon), dtype='datetime64[s]')
+        mstHours=np.empty([1,len(lon)]).astype('float64')
+        for ii in range(0,len(lon)):
+            mst[ii]=time+datetime.timedelta(minutes=lon[ii]*4)
+            mstHours[0,ii]=mst[ii].astype(object).hour
+            
+        mstHours2D=np.repeat(mstHours,len(lat),0)
+        
+        echoType2Din=np.squeeze(readThis.variables['EchoTypeComp'][:])
+        echoType2Din=echoType2Din.filled(fill_value=np.nan)
+        
+        conv2Din=np.squeeze(readThis.variables['ConvectivityComp'][:])
+        #conv2Din=conv2Din.filled(fill_value=np.nan)
+        
+        for ii in range(24):
+            #print(ii)
+            convThis=np.where(((echoType2Din==14) & (mstHours2D==ii)),conv2Din,0);
+            StratLow[ii,:,:]=StratLow[ii,:,:]+convThis
+            convThis=np.where(((echoType2Din==16) & (mstHours2D==ii)),conv2Din,0);
+            StratMid[ii,:,:]=StratMid[ii,:,:]+convThis
+            convThis=np.where(((echoType2Din==18) & (mstHours2D==ii)),conv2Din,0);
+            StratHigh[ii,:,:]=StratHigh[ii,:,:]+convThis
+            convThis=np.where(((echoType2Din==25) & (mstHours2D==ii)),conv2Din,0);
+            Mixed[ii,:,:]=Mixed[ii,:,:]+convThis
+            convThis=np.where(((echoType2Din==32) & (mstHours2D==ii)),conv2Din,0);
+            ConvElev[ii,:,:]=ConvElev[ii,:,:]+convThis
+            convThis=np.where(((echoType2Din==34) & (mstHours2D==ii)),conv2Din,0);
+            ConvShallow[ii,:,:]=ConvShallow[ii,:,:]+convThis
+            convThis=np.where(((echoType2Din==36) & (mstHours2D==ii)),conv2Din,0);
+            ConvMid[ii,:,:]=ConvMid[ii,:,:]+convThis
+            convThis=np.where(((echoType2Din==38) & (mstHours2D==ii)),conv2Din,0);
+            ConvDeep[ii,:,:]=ConvDeep[ii,:,:]+convThis
+            
+            CountAllEcho[ii,:,:]=CountAllEcho[ii,:,:]+((~np.isnan(echoType2Din)) & (mstHours2D==ii)).astype(int)
+            CountAllHours[ii,:,:]=CountAllHours[ii,:,:]+(mstHours2D==ii).astype(int)
+    
+    echoType2D={
+        "CountAllHours":CountAllHours,
+        "CountAllEcho":CountAllEcho,
+        "lon":lon,
+        "lat":lat,
+        "StratLowConvectivity":StratLow,
+        "StratMidConvectivity":StratMid,
+        "StratHighConvectivity":StratHigh,
+        "MixedConvectivity":Mixed,
+        "ConvElevConvectivity":ConvElev,
+        "ConvShallowConvectivity":ConvShallow,
+        "ConvMidConvectivity":ConvMid,
+        "ConvDeepConvectivity":ConvDeep
+    }
+    
+    outfile=options.outDir+'mrmsStatsConvectivity_'+startTime.strftime("%Y%m%d")+'_to_'+endTime.strftime("%Y%m%d")+'.pickle'
+    with open(outfile, 'wb') as handle:
+        pickle.dump(echoType2D, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+########################################################################
+# Run - entry point
+
+if __name__ == "__main__":
+   main()
+
---
+++ ecco/dataProcessing/plot_mrmsStats_categories_conv.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+
+#===========================================================================
+#
+# Process MRMS ECCO output
+#
+#===========================================================================
+
+import sys
+from optparse import OptionParser
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pl
+import cartopy
+import cartopy.feature as cfeature
+import cartopy.crs as ccrs
+from cartopy.util import add_cyclic_point
+#import netCDF4 as nc
+#from matplotlib import dates
+#import datetime
+#import pathlib
+#from matplotlib.dates import DateFormatter
+#import pandas as pd
+import pickle
+from mpl_toolkits.basemap import Basemap
+
+def main():
+
+    plt.close('all')
+    
+    indir='/scr/cirrus2/rsfdata/projects/nexrad-mrms/statMats/'
+    infile='mrmsStatsConvectivity_20220501_to_20220501.pickle'
+    infile2='mrmsStatsConvectivity_20220501_to_20220501.pickle'
+    
+    figdir='/scr/cirrus2/rsfdata/projects/nexrad-mrms/figures/eccoStats/'
+    
+    with open(indir+infile, 'rb') as handle:
+        conv2D=pickle.load(handle)
+        
+    with open(indir+infile2, 'rb') as handle:
+        echoType2D=pickle.load(handle)
+    
+    xlims=[min(conv2D['lon']),max(conv2D['lon'])]
+    ylims=[min(conv2D['lat']),max(conv2D['lat'])]
+    
+    resol = '50m'
+    provinc_bodr = cartopy.feature.NaturalEarthFeature(category='cultural', 
+        name='admin_1_states_provinces_lines', scale=resol, facecolor='none', edgecolor='k')
+   
+    fig, ax= plt.subplots(4,2,subplot_kw={'projection': ccrs.PlateCarree()},figsize=(12,12),constrained_layout=True)
+    ax=ax.ravel()
+    ii=0
+    for jj, (key,value) in enumerate(conv2D.items()):
+        if key not in ['CountAllHours','CountAllEcho','lon','lat']:
+            valSum=np.sum(value,0)
+            valSum[valSum==0]='nan'
+            echoSum=np.sum(echoType2D[key[0:-13]],0)
+            echoSum[echoSum==0]='nan'
+            valNorm=valSum/echoSum
+            clevs=np.arange(0,np.nanpercentile(valNorm,100),0.01)
+            
+            cs=ax[ii].contourf(conv2D['lon'], conv2D['lat'], valNorm,
+                clevs,cmap='turbo',extend='both')
+            ax[ii].coastlines()
+            ax[ii].add_feature(cfeature.BORDERS)
+            ax[ii].add_feature(provinc_bodr, linestyle='--', linewidth=0.6, edgecolor="k", zorder=10)
+            ax[ii].set_xticks(np.arange(xlims[0],xlims[1]+1,10))
+            ax[ii].set_yticks(np.arange(ylims[0],ylims[1]+1,10))
+            ax[ii].set_title(key,fontweight='bold')
+            ii=ii+1
+                
+    fig.colorbar(cs, ax=ax.ravel().tolist(), shrink=0.4, fraction=0.5)
+    plt.show()
+    
+    plt.savefig(figdir+'categoriesConvectivity_'+infile[-27:-7]+'.png')
+    
+    plt.close(fig)
+               
+########################################################################
+# Run - entry point
+
+if __name__ == "__main__":
+   main()
+
