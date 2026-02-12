1bd45990afa01d9b5f63ec73df17c2ddb1e461ee
leavesntwigs <bjav7496@gmail.com>
2026-02-12 13:16:47 -0700

    adding test for fill region section of f_velTexture.m

---
+++ ecco-v-test/fillRegionsClosestPixel_test.py
@@ -0,0 +1,103 @@
+import unittest
+import numpy as np
+import sys
+sys.path.insert(1, '../ecco-v-py')
+import pullOutWindow as pw
+
+class Test_velTexture_Methods(unittest.TestCase):
+
+    @unittest.skip("demonstrating skipping")
+    def test_zero(self):
+        VEL=[0,0,0,0]
+        pixRad = 2  # int between 0 and len(VEL) ??
+        velBase = -1   # base value for velocities; this is subtracted from VEL; must have same dimensions as VEL? or not???
+        velText = f_velTexture.f_velTexture(VEL, pixRad, velBase)
+        velExpect = [0,0,0,np.Nan]
+        self.assertEqual(velActual, velExpect)
+
+    @unittest.skip("demonstrating skipping")
+    def test_rand(self):
+        VEL = np.array([
+            [79.,  9., 78., 13.],
+            [12., 97., 48., 30.],
+            [85., 48., 60.,  5.],
+            [39., 57., 71., 79.]])
+        pixRad = 2  # int between 0 and len(VEL) ??
+        velBase = 1   # base value for velocities; this is subtracted from VEL; must have same dimensions as VEL? or not???
+        velActual = f_velTexture.f_velTexture(VEL, pixRad, velBase)
+        print(velActual)
+        velExpect = np.array([
+            [40.38800768, 40.38800768, 32.37019165, 32.37019165],
+            [40.38800768,      np.nan,      np.nan, 32.37019165],
+            [40.38800768, 40.38800768, 32.37019165, 32.37019165],
+            [40.38800768, 40.38800768, 32.37019165, 32.37019165]])
+        # velExpect_matlab = np.array([...
+        decimal = 1e-5
+        result = np.allclose(velActual, velExpect, decimal, equal_nan = True)
+        # self.assertAlmostEqual(velActual, velExpect, decimal, "not equal")
+        self.assertTrue(result, "not equal")
+
+    def test_scipy_cKDTree_one_component(self):
+        import numpy as np
+        import cv2 as cv
+        import pullOutWindow as pw
+        import f_velTexturePre as vtp
+        import fillRegionsClosestPixel as frcp
+
+        VEL = np.array([[0,0,0,0,0], [3.5377,3.8622,2.5664,0,0], [4.8339,3.3188,3.3426,0,0], [0.7412,1.6923,6.5784,0,0], [0,0,0,0,0]])
+        pixRad = 2
+        velBase = 0
+        VELmask, VELsmall, velText = vtp.f_velTexture(VEL,pixRad,velBase)
+
+        retval, labels = cv.connectedComponents(VELmask)
+        velActual = frcp.fillRegionsClosestPixel(VELmask, VELsmall, velText, VEL)
+        velExpect = np.array([
+            [0.        , 0.        , 0.        , 0.        ,        nan],
+            [1.70757646, 2.27440453, 1.68817099, 1.18394518,        nan],
+            [1.89013863, 2.43437944, 1.88271026, 1.41732399,        nan],
+            [2.59920887, 4.0022225 , 4.33711156, 3.63572722,        nan],
+            [0.        , 0.        , 0.        , 0.        ,        nan]])
+
+        print("velActual = ")
+        print(velActual)
+
+        print("velExpect = ")
+        print(velExpect)
+
+        decimal = 1e-5
+        result = np.allclose(velActual, velExpect, decimal, equal_nan = True)
+        # self.assertAlmostEqual(velActual, velExpect, decimal, "not equal")
+        self.assertTrue(result, "not equal")
+
+    @unittest.skip("demonstrating skipping")
+    def test_10x10_ones_pixRad_2_velBase_0(self):
+        VEL = np.ones((10,10))
+        pixRad = 2  # int between 0 and len(VEL) ??
+        velBase = 0   # base value for velocities; this constant is subtracted from VEL
+        velActual = f_velTexture.f_velTexture(VEL, pixRad, velBase)
+        print("velActual = ")
+        print(velActual)
+
+#
+#         velText
+# array([[ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., nan],
+#        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., nan],
+#        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., nan],
+#        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., nan],
+#        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., nan],
+#        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., nan],
+#        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., nan],
+#        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., nan],
+#        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., nan],
+#        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0., nan]])
+#
+
+        velExpect = np.zeros((10,10))
+        velExpect[:,9] = np.nan
+        decimal = 1e-5
+        result = np.allclose(velActual, velExpect, decimal, equal_nan = True)
+        # self.assertAlmostEqual(velActual, velExpect, decimal, "not equal")
+        self.assertTrue(result, "not equal")
+
+if __name__ == '__main__':
+    unittest.main()
